8ba335c0e04fcae4c3812946581093141c6225e0
Aleksandr Tikhonov <stellar.irk@gmail.com>
2026-01-28 18:47:47 +0400

    chore(deploy): Add DB install script

--- deploy/dbInstall.php
+++ deploy/dbInstall.php
@@ -2,10 +2,11 @@
 echo("Zoneminder DB Install script");
 
 $mjdm_dir = '/var/www/html/';
+chdir($mjdm_dir);
 include_once($mjdm_dir."config.php");
 include_once($mjdm_dir."lib/loader.php");
 include_once($mjdm_dir."load_settings.php");
 include_once($mjdm_dir."modules/zoneminder/zoneminder.class.php");
 
 $zm = new zoneminder();
-$zm->dbInstall();
+$zm->dbInstall('');
